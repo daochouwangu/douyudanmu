a08b2b82943032f58765f3c15b767e550d736cda
lyb <daochouwangu@gmail.com>
2019-05-30 19:56:38 +0800

    Update douyudanmu.js
    把弹幕放入播放器中

--- douyudanmu.js
+++ douyudanmu.js
@@ -11,8 +11,7 @@ var callback = function(mutationsList) {
 var observer = new MutationObserver(callback);
 observer.observe(targetNode, config);
 //建立一个放弹幕的容器
-var wrapper = document.createElement('div')
-document.body.append(wrapper)
+var wrapper = document.querySelector("#__h5player").children[3].children[1]
 //使用第一行弹幕的时间来控制弹幕的并发
 var startTime = 0
 var MIN_TIME = 1000
@@ -23,10 +22,11 @@ function send(danmu) {
 	if(!danmu) return;
 	var dom = danmu.cloneNode(true)
 	var now = new Date()
-	dom.style.position = 'fixed'
+  var clientWidth = wrapper.clientWidth
+	dom.style.position = 'absolute'
 	dom.style.zIndex = '10000'
 	dom.style.transition = 'transform 7s linear'
-	dom.style.transform = 'translateX(200px)'
+	dom.style.transform = `translateX(${clientWidth}px)`
 	dom.style.fontSize = '20px'
 	dom.style.fontWeight = '800'
 	if (!danmu.getAttribute('class').includes('color')){
@@ -48,11 +48,10 @@ function send(danmu) {
 		}
 	}
 	dom.style.top = `${top}px`
-	dom.style.right='0'
 	wrapper.append(dom)
-	var clientWidth = document.body.clientWidth
 	//弹幕滚动效果
-	setTimeout(function(){dom.style.transform = `translateX(-${clientWidth}px)`},0)
+	setTimeout(function(){dom.style.transform = 'translateX(-200px)'},0)
 	//垃圾回收
 	setTimeout(function(){dom.remove()},7000)
 }
+setTimeout(function(){observer.disconnect()},6000)

213a7dbd55317faffb5e212249f557037f46262e
lyb <daochouwangu@gmail.com>
2019-05-31 22:55:25 +0800

    Update douyudanmu.js
    使个性配置更加简单
    增加了弹幕速度的个性配置
    无论播放器大小现在弹幕都是匀速运动
    修复了弹幕太多会显示在播放器下方的bug

--- douyudanmu.js
+++ douyudanmu.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         斗鱼弹幕显示
 // @namespace    https://github.com/daochouwangu/douyudanmu/blob/master/douyudanmu.js
-// @version      1.00
+// @version      1.10
 // @description  显示弹幕
 // @supportURL   https://github.com/daochouwangu/douyudanmu/issues
 // @author       lyb
@@ -13,6 +13,11 @@
 
 (function() {
     'use strict';
+    var FONT_SIZE = 20	//字体的大小
+    var DANMU_PADDING = 10 //弹幕的宽度
+    var SPEED = 200 //弹幕速度
+    var MIN_TIME = 2000 //同一行的弹幕最小间隔时间，如果弹幕有重叠就增加这个值
+    var BEGIN_TOP = 10 //弹幕离最上方的距离
     var start = setInterval(function(){
         if (document.querySelector("#__h5player").children[3].children[1]){
             initApplication()
@@ -20,6 +25,8 @@
         }
     },500)
     function initApplication(){
+        var startTime = 0
+        var prevTop = 0
         var targetNode = document.getElementById('js-player-barrage');
         var config = { attributes: false, childList: true, subtree: true };
         var callback = function(mutationsList) {
@@ -34,21 +41,18 @@
         //建立一个放弹幕的容器
         var wrapper = document.querySelector("#__h5player").children[3].children[1]
         //使用第一行弹幕的时间来控制弹幕的并发
-        var startTime = 0
-        var MIN_TIME = 1000
-        var prevTop = 0
-        var FONT_SIZE = 50	//弹幕的宽度
-        var BEGIN_TOP = 70 //弹幕离最上方的距离
         function send(danmu) {
             if(!danmu) return;
             var dom = danmu.cloneNode(true)
             var now = new Date()
             var clientWidth = wrapper.clientWidth
+            var clientHeight = wrapper.clientHeight
+            var time = clientWidth / SPEED
             dom.style.position = 'absolute'
             dom.style.zIndex = '10000'
-            dom.style.transition = 'transform 7s linear'
+            dom.style.transition = `transform ${time}s linear`
             dom.style.transform = `translateX(${clientWidth}px)`
-            dom.style.fontSize = '20px'
+            dom.style.fontSize = `${FONT_SIZE}px`
             dom.style.fontWeight = '800'
             if (!danmu.getAttribute('class').includes('color')){
                 dom.style.color = '#fff'
@@ -58,9 +62,9 @@
             if (startTime === 0 ) {
                 startTime = now
             } else {
-                if(now - startTime <= MIN_TIME) {
-                    top=prevTop + FONT_SIZE
-                    prevTop = prevTop + FONT_SIZE
+                if(now - startTime <= MIN_TIME && prevTop + 2 * FONT_SIZE + DANMU_PADDING < clientHeight) {
+                    top=prevTop + FONT_SIZE + DANMU_PADDING
+                    prevTop = prevTop + FONT_SIZE + DANMU_PADDING
                 } else {
                     top=BEGIN_TOP
                     prevTop = BEGIN_TOP
@@ -72,7 +76,7 @@
             //弹幕滚动效果
             setTimeout(function(){dom.style.transform = 'translateX(-200px)'},0)
             //垃圾回收
-            setTimeout(function(){dom.remove()},7000)
+            setTimeout(function(){dom.remove()},time * 1000)
         }
     }
 })();
